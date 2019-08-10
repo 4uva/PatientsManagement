7406f6c5b9f5c2855c6e0634614c4e7c5de57a07
4uva <41539104+4uva@users.noreply.github.com>
2019-08-11 00:31:30 +0300

    Added migration

---
+++ PatientsManagement/Migrations/20190810212912_InitialCreate.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PatientsManagement.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Patients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Surname = table.Column<string>(maxLength: 128, nullable: false),
+                    Name = table.Column<string>(maxLength: 128, nullable: false),
+                    Patronymic = table.Column<string>(maxLength: 128, nullable: true),
+                    PrimaryPhone = table.Column<string>(maxLength: 13, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patients", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AdditionalContact",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Kind = table.Column<int>(nullable: false),
+                    Phone = table.Column<string>(maxLength: 13, nullable: false),
+                    PatientId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AdditionalContact", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AdditionalContact_Patients_PatientId",
+                        column: x => x.PatientId,
+                        principalTable: "Patients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AdditionalContact_PatientId",
+                table: "AdditionalContact",
+                column: "PatientId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AdditionalContact");
+
+            migrationBuilder.DropTable(
+                name: "Patients");
+        }
+    }
+}
